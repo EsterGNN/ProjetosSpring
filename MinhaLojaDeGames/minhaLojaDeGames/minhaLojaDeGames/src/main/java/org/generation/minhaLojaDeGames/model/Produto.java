570abe23625e72aa3e5a2c83e6176227116604f4
EsterGNN <gomes.ester23@gmail.com>
2021-08-30 12:57:31 -0300

    Atividade CRUD Back-end Minha Loja de Games

---
+++ MinhaLojaDeGames/minhaLojaDeGames/minhaLojaDeGames/src/main/java/org/generation/minhaLojaDeGames/model/Produto.java
@@ -0,0 +1,77 @@
+package org.generation.minhaLojaDeGames.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_produto")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min = 5, max = 40)
+	private String nome;
+	
+	@NotNull
+	@Size(max = 500)
+	private String sinopse;
+	
+	@NotNull
+	private long idadeMinima;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSinopse() {
+		return sinopse;
+	}
+
+	public void setSinopse(String sinopse) {
+		this.sinopse = sinopse;
+	}
+
+	public long getIdadeMinima() {
+		return idadeMinima;
+	}
+
+	public void setIdadeMinima(long idadeMinima) {
+		this.idadeMinima = idadeMinima;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+}
